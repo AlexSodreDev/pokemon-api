78e58a2718ba39ca0f99f4f55eff977460001e51
AlexSodreDev <alexandre.cortines@gmail.com>
2022-02-10 10:38:58 -0300

    add get new routes

--- src/routes/pokemon.routes.ts
+++ src/routes/pokemon.routes.ts
@@ -6,7 +6,13 @@ const router = Router()
 const pokemonController = new PokemonController()
 
 router.get('/hello', pokemonController.helloWorld)
+router.get('/pokemons', pokemonController.index)
+router.get('/pokemons/:id', pokemonController.findById)
 
 router.post('/pokemons', pokemonController.create)
+
+router.delete('/pokemons/:id', pokemonController.delete)
+
+router.patch('/pokemons/:id', pokemonController.update)
 
 export default router
